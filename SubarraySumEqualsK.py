a06e7f67f9db7c956dd3db61be8f037d20feed47
belphegor32 <155807528+belphegor32@users.noreply.github.com>
2024-01-28 17:16:09 +0300

    just a commit

--- SubarraySumEqualsK.py
+++ SubarraySumEqualsK.py
@@ -1,16 +1,21 @@
 from collections import defaultdict
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
+        # make a count dict to keep a track of prefix sums
         count = defaultdict(int)
+        # base case in the dict for prefix sum = 0
         count[0] = 1
         res = 0 
         cur = 0
 
-        for n in nums:
+        for n in nums: 
             cur += n
+            # find the diffrence to search for how many arrays can we use to satisfy the res 
             dif = cur - k
 
+            # search and add the number of arrays with prefix sum dif
             res += count[dif]
+            # increment the prefix sum dict value at key cur, since we just understood, that it satisfies the value we search for
             count[cur] += 1
         
         return res
