1a5deeaaa231639e6731aacd24e4a58119c5e5cf
belphegor32 <155807528+belphegor32@users.noreply.github.com>
2024-02-03 13:22:55 +0300

    daily challenge

--- python/PartitionArrayForMaximumSum.py
+++ python/PartitionArrayForMaximumSum.py
@@ -24,3 +24,28 @@ class Solution:
             return res
 
         return rec(0)
+
+# circular array solution from editorial
+class Solution2:
+    def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
+        dp = [0] * k
+        dp[0] = arr[0]
+
+        for i in range(1, len(arr)):
+            cur_max = 0
+            max_at_cur_index = 0
+            for j in range(i, i - k, - 1):
+                if j < 0:
+                    break
+                cur_max = max(cur_max, arr[j])
+                window_size = i - j + 1
+                cur_sum = cur_max * window_size
+                if j > 0:
+                    sub_sum = dp[(j - 1) % k]
+                else:
+                    sub_sum = dp[-1]
+                max_at_cur_index = max(max_at_cur_index, cur_sum + sub_sum)
+
+            dp[i % k] = max_at_cur_index
+        
+        return dp[(len(arr) - 1) % k]
