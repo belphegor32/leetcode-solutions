3925ed3ee8785d482dcc8a25b750f74f3491b85a
belphegor32 <155807528+belphegor32@users.noreply.github.com>
2024-03-18 08:32:17 +0300

    daily challenge

---
+++ python/MinimumNumberOfArrowsToBurstBallons.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        points.sort()
+
+        res = len(points)
+        prev = points[0]
+        for i in range(1, len(points)):
+            cur = points[i]
+
+            # if overlap, then we can burst to ballons at once
+            if cur[0] <= prev[1]:
+                res -= 1
+                prev[0] = min(prev[0], cur[0])
+                prev[1] = min(prev[1], cur[1])
+            # if there is no overlap we just move on
+            elif cur[0] > prev[1]:
+                prev = cur
+
+        return res
